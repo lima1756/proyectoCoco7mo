76becd9de00f67ce3fe2ea23d78fc45a00ab25cc
BRSADT <brendasamant@hotmail.com>
2016-12-03 17:08:23 -0600

    creo que ya verifica la cadena de xml

--- proyecto/proyecto/Conectar.vb
+++ proyecto/proyecto/Conectar.vb
@@ -1,23 +1,45 @@
-﻿Public Class Conectar
-
+﻿Imports System.Data.SqlClient
+Public Class Conectar
+    Private conexion As SqlConnection
+    Public idUsuario As String
+    Public err As Boolean = False
     Dim Cnt As New DataSet("Conexion")
 
     Private Sub Conectar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Cnt.ReadXml("Conexion.xml")
         TextBox1.DataBindings.Add("Text", Cnt, "Conexion.Cadena")
+        Dim intento As String
+        intento = TextBox1.Text
+
+        Try
+            conexion = New SqlConnection(intento)
+            conexion.Open()
+
+        Catch ex As Exception
+            err = True
+            MessageBox.Show("Error de conexion")
+        End Try
 
 
-        If Inicio.err = False Then
-            Inicio.Show()
-            Me.Finalize()
-        End If
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Cnt.WriteXml("Conexion.xml")
+        Dim intento As String
+        intento = TextBox1.Text
+        Try
+            conexion = New SqlConnection(intento)
+            conexion.Open()
+            Cnt.WriteXml("Conexion.xml")
 
-        Me.Finalize()
-        Inicio.Show()
+            Me.Finalize()
+            Inicio.Show()
+        Catch ex As Exception
+            err = True
+            MessageBox.Show("la cadena de conexion no es valida")
+        End Try
+
+
     End Sub
 
 
